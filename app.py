7478797c39742e73e31c90b634dc61b81eedb88d
ronnitburman <ronnitburman@gmail.com>
2024-09-09 04:02:17 -0400

    initial code commit

--- app.py
+++ app.py
@@ -8,11 +8,56 @@ from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import time
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
+groq_api_key = os.getenv('GROQ_API_KEY')
+os.environ['GOOGLE_API_KEY'] = os.getenv('GOOGLE_API_KEY')
+
 st.title("Document_RAG")
-st.text_input("enter path")
-st.button("Create Embeddings")
+
+llm = ChatGroq(groq_api_key=groq_api_key, model_name="Gemma-7b-it")
+
+prompt = ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question.
+
+    <context>
+    {context}
+    <context>
+
+    Question: {input}
+    """
+)
+
+def vector_embedding():
+    if "vectors" not is st.session_state:
+        st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        st.session_state.loader=PyPDFDirectoryLoader("./Document")
+        st.session_state.docs = st.session_state.loader.load()
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+        st.session_state.final_document = st.session_state.text_splitter.split_documents(st.session_state.docs)
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)
+
+prompt1=st.text_input("Type your question here")
+if st.button("Create Embeddings"):
+    vector_embedding()
+    st.write("Vector Store is ready")
+
+if prompt1:
+    document_chain=create_stuff_documents_chain(llm=llm)
+    retriever=st.session_state.vectors.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+    start=time.process_time()
+    response=retrieval_chain.invoke({"input":prompt1})
+    st.write(response)
+
+    with st.expander("Document Similarity Search"):
+        for i,doc in enumerate(response['context']):
+            st.write(doc.page_content)
+            st.write("-----------------------------------------")
