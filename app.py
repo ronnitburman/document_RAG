6dce95311442ec432b65a8e64b34fdf002e909cd
ronnitburman <ronnitburman@gmail.com>
2024-09-09 03:25:22 -0400

    created app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+st.title("Document_RAG")
+st.text_input("enter path")
+st.button("Create Embeddings")
